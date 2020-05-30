e1adfd2d89e5a60223c26530b7fe1670b74e4ea6
FreeGamers <contact@freegamers.org>
2020-05-30 18:28:27 -0500

    fix gold coins, update todo list

--- mods/coins/crafting.lua
+++ mods/coins/crafting.lua
@@ -2,6 +2,26 @@
 minetest.register_craft({
 	type = "cooking",
 	cooktime = "60",
-	output = "coins:gold_coins 100",
+	output = "coins:gold_coinss 8",
 	recipe = "default:gold_ingot",
+})
+
+-- Use a bucket of coins to smelt coins back into ingots.
+minetest.register_craft({
+	type = "shapeless",
+	output = "coins:gold_scrap_bucket",
+	recipe = {
+		"coins:gold_coins", "coins:gold_coins","coins:gold_coins",
+		"coins:gold_coins", "bucket:bucket_empty","coins:gold_coins",
+		"coins:gold_coins", "coins:gold_coins","coins:gold_coins"
+	    	}
+})
+
+-- Cook a bucket of 8 gold coins to smelt gold ingot.
+minetest.register_craft({
+	type = "cooking",
+	cooktime = "60",
+	output = "default:gold_ingot",
+	recipe = "coins:gold_scrap_bucket",
+	replacements = {{"coins:gold_scrap_bucket","bucket:bucket_empty"}}
 })
--- mods/coins/init.lua
+++ mods/coins/init.lua
@@ -1,5 +1,6 @@
 -- TODO: coin press machine? (copper, silver) coins? melt coins back to ingots.
 
+-- Get mod path and execute additional modules.
 local path = minetest.get_modpath("coins")
 dofile(path .. "/crafting.lua")
 
@@ -7,5 +8,12 @@ dofile(path .. "/crafting.lua")
 minetest.register_craftitem("coins:gold_coins", {
         description = "Gold Coins",
         inventory_image = "coins_gold_coins.png",
-        stack_max = "100",
+        stack_max = "99",
+})
+
+-- Register the gold coin scrap bucket.
+minetest.register_craftitem("coins:gold_scrap_bucket", {
+        description = "Bucket of Gold Scrap",
+        inventory_image = "coins_bucket_scrap_gold.png",
+        stack_max = 1,
 })
