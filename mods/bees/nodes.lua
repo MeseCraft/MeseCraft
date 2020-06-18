cdd96e088d862b4c01be661d718f96acb504c969
FreeGamers <contact@freegamers.org>
2020-06-18 14:13:04 -0500

    improve honeyblock preview image

--- mods/bees/nodes.lua
+++ mods/bees/nodes.lua
@@ -2,7 +2,6 @@
 
 minetest.register_node("bees:honeycomb_block", {
         description = "Honeycomb Block",
-        inventory_image = "bees_honeycomb_block.png",
         tiles = {"bees_honeycomb_block.png"},
         groups = {oddly_breakable_by_hand = 3, dig_immediate = 1},
         sounds = default.node_sound_dirt_defaults(),
