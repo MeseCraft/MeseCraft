41b8269e6d8e7d3134bd413ed17383e3046b7a62
MeseCraft <contact@mesecraft.com>
2022-01-18 02:32:22 -0600

    adjust announce to 20 minutes (from 10).

--- mods/HUD/announce/config.lua
+++ mods/HUD/announce/config.lua
@@ -1,5 +1,5 @@
 return {
-    delay = 600, -- in seconds
+    delay = 1200, -- in seconds
     announcements = {
 	"&eMESECRAFT: &2MeseCraft is currently in alpha stage of development. Expect major changes!",
 	"&eMESECRAFT: &2Join the MeseCraft online community at &ewww.mesecraft.com",
